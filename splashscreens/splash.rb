0efe5e1dce105b38ea1f8a6b1d36988e7b3d5005
disc0ninja <disc0ninja77@gmail.com>
2016-09-23 11:10:08 -0500

    Wrote a simple script to create new dir named after each image file in the current folder

---
+++ splashscreens/splash.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+puts "Welcome to disc0ninja's Splashscreen helper"
+puts
+puts "Are you running this tool from your splashscreens folder? (y/n)"
+puts
+ans = gets.chomp
+
+if ans != "y"
+  puts
+  puts "Please move this script into your splashscreens directory and try again"
+else
+  # store the current dir
+  current_dir = Dir.getwd
+  puts current_dir
+  # create an empty array to store all the image files in
+  new_dirs = []
+  images = Dir.glob("*.{jpg,png}")
+  images.each do |image|
+    new_dirs.push(image.gsub(/(\.).*/, "").strip.upcase)
+    puts new_dirs
+  end
+  new_dirs.each {
+    |dirname| puts "Creating new dir: " + current_dir + "/" + dirname
+  Dir.mkdir(dirname)
+  }
+end
